3815beee3aafde3ff8d512bc619026b849f94112
杨晨光 <ycg1024@qq.com>
2016-04-09 22:13:04 +0800

    2016/04/09

---
+++ SerialAssistant/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace SerialAssistantApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gpbCOMSet = new System.Windows.Forms.GroupBox();
+            this.COMCheck = new System.Windows.Forms.Button();
+            this.About = new System.Windows.Forms.Button();
+            this.rbnHEX = new System.Windows.Forms.RadioButton();
+            this.rbnChar = new System.Windows.Forms.RadioButton();
+            this.btnOpenCOM = new System.Windows.Forms.Button();
+            this.cbbParity = new System.Windows.Forms.ComboBox();
+            this.cbbStopBits = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbbDataBits = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbbBaudRate = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbbCOMPort = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnClearData = new System.Windows.Forms.Button();
+            this.gpbDataRev = new System.Windows.Forms.GroupBox();
+            this.tbxDataRev = new System.Windows.Forms.TextBox();
+            this.gpbDataSend = new System.Windows.Forms.GroupBox();
+            this.btnSendData = new System.Windows.Forms.Button();
+            this.tbxDataSend = new System.Windows.Forms.TextBox();
+            this.gpbCOMSet.SuspendLayout();
+            this.gpbDataRev.SuspendLayout();
+            this.gpbDataSend.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gpbCOMSet
+            // 
+            resources.ApplyResources(this.gpbCOMSet, "gpbCOMSet");
+            this.gpbCOMSet.Controls.Add(this.COMCheck);
+            this.gpbCOMSet.Controls.Add(this.About);
+            this.gpbCOMSet.Controls.Add(this.rbnHEX);
+            this.gpbCOMSet.Controls.Add(this.rbnChar);
+            this.gpbCOMSet.Controls.Add(this.btnOpenCOM);
+            this.gpbCOMSet.Controls.Add(this.cbbParity);
+            this.gpbCOMSet.Controls.Add(this.cbbStopBits);
+            this.gpbCOMSet.Controls.Add(this.label4);
+            this.gpbCOMSet.Controls.Add(this.cbbDataBits);
+            this.gpbCOMSet.Controls.Add(this.label3);
+            this.gpbCOMSet.Controls.Add(this.cbbBaudRate);
+            this.gpbCOMSet.Controls.Add(this.label2);
+            this.gpbCOMSet.Controls.Add(this.cbbCOMPort);
+            this.gpbCOMSet.Controls.Add(this.label1);
+            this.gpbCOMSet.Controls.Add(this.label5);
+            this.gpbCOMSet.Name = "gpbCOMSet";
+            this.gpbCOMSet.TabStop = false;
+            // 
+            // COMCheck
+            // 
+            resources.ApplyResources(this.COMCheck, "COMCheck");
+            this.COMCheck.Name = "COMCheck";
+            this.COMCheck.UseVisualStyleBackColor = true;
+            this.COMCheck.Click += new System.EventHandler(this.COMCheck_Click);
+            // 
+            // About
+            // 
+            resources.ApplyResources(this.About, "About");
+            this.About.Name = "About";
+            this.About.UseVisualStyleBackColor = true;
+            this.About.Click += new System.EventHandler(this.About_Click);
+            // 
+            // rbnHEX
+            // 
+            resources.ApplyResources(this.rbnHEX, "rbnHEX");
+            this.rbnHEX.Name = "rbnHEX";
+            this.rbnHEX.UseVisualStyleBackColor = true;
+            // 
+            // rbnChar
+            // 
+            resources.ApplyResources(this.rbnChar, "rbnChar");
+            this.rbnChar.Checked = true;
+            this.rbnChar.Name = "rbnChar";
+            this.rbnChar.TabStop = true;
+            this.rbnChar.UseVisualStyleBackColor = true;
+            
+            // 
+            // btnOpenCOM
+            // 
+            resources.ApplyResources(this.btnOpenCOM, "btnOpenCOM");
+            this.btnOpenCOM.Name = "btnOpenCOM";
+            this.btnOpenCOM.UseVisualStyleBackColor = true;
+            this.btnOpenCOM.Click += new System.EventHandler(this.btnOpenCOM_Click);
+            // 
+            // cbbParity
+            // 
+            this.cbbParity.FormattingEnabled = true;
+            resources.ApplyResources(this.cbbParity, "cbbParity");
+            this.cbbParity.Name = "cbbParity";
+            // 
+            // cbbStopBits
+            // 
+            this.cbbStopBits.FormattingEnabled = true;
+            resources.ApplyResources(this.cbbStopBits, "cbbStopBits");
+            this.cbbStopBits.Name = "cbbStopBits";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // cbbDataBits
+            // 
+            this.cbbDataBits.FormattingEnabled = true;
+            resources.ApplyResources(this.cbbDataBits, "cbbDataBits");
+            this.cbbDataBits.Name = "cbbDataBits";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // cbbBaudRate
+            // 
+            this.cbbBaudRate.FormattingEnabled = true;
+            resources.ApplyResources(this.cbbBaudRate, "cbbBaudRate");
+            this.cbbBaudRate.Name = "cbbBaudRate";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // cbbCOMPort
+            // 
+            this.cbbCOMPort.FormattingEnabled = true;
+            resources.ApplyResources(this.cbbCOMPort, "cbbCOMPort");
+            this.cbbCOMPort.Name = "cbbCOMPort";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // btnClearData
+            // 
+            resources.ApplyResources(this.btnClearData, "btnClearData");
+            this.btnClearData.Name = "btnClearData";
+            this.btnClearData.UseVisualStyleBackColor = true;
+            this.btnClearData.Click += new System.EventHandler(this.btnClearData_Click);
+            // 
+            // gpbDataRev
+            // 
+            this.gpbDataRev.Controls.Add(this.tbxDataRev);
+            resources.ApplyResources(this.gpbDataRev, "gpbDataRev");
+            this.gpbDataRev.Name = "gpbDataRev";
+            this.gpbDataRev.TabStop = false;
+            // 
+            // tbxDataRev
+            // 
+            resources.ApplyResources(this.tbxDataRev, "tbxDataRev");
+            this.tbxDataRev.Name = "tbxDataRev";
+            this.tbxDataRev.ReadOnly = true;
+            // 
+            // gpbDataSend
+            // 
+            this.gpbDataSend.Controls.Add(this.btnSendData);
+            this.gpbDataSend.Controls.Add(this.tbxDataSend);
+            this.gpbDataSend.Controls.Add(this.btnClearData);
+            resources.ApplyResources(this.gpbDataSend, "gpbDataSend");
+            this.gpbDataSend.Name = "gpbDataSend";
+            this.gpbDataSend.TabStop = false;
+            // 
+            // btnSendData
+            // 
+            resources.ApplyResources(this.btnSendData, "btnSendData");
+            this.btnSendData.Name = "btnSendData";
+            this.btnSendData.UseVisualStyleBackColor = true;
+            this.btnSendData.Click += new System.EventHandler(this.btnSendData_Click);
+            // 
+            // tbxDataSend
+            // 
+            resources.ApplyResources(this.tbxDataSend, "tbxDataSend");
+            this.tbxDataSend.Name = "tbxDataSend";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.gpbDataSend);
+            this.Controls.Add(this.gpbDataRev);
+            this.Controls.Add(this.gpbCOMSet);
+            this.Name = "Form1";
+            this.Opacity = 0.95D;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gpbCOMSet.ResumeLayout(false);
+            this.gpbCOMSet.PerformLayout();
+            this.gpbDataRev.ResumeLayout(false);
+            this.gpbDataRev.PerformLayout();
+            this.gpbDataSend.ResumeLayout(false);
+            this.gpbDataSend.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox gpbCOMSet;
+        private System.Windows.Forms.GroupBox gpbDataRev;
+        private System.Windows.Forms.GroupBox gpbDataSend;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbbParity;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cbbStopBits;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cbbDataBits;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbbBaudRate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbbCOMPort;
+        private System.Windows.Forms.RadioButton rbnHEX;
+        private System.Windows.Forms.RadioButton rbnChar;
+        private System.Windows.Forms.Button btnClearData;
+        private System.Windows.Forms.Button btnOpenCOM;
+        private System.Windows.Forms.TextBox tbxDataRev;
+        private System.Windows.Forms.TextBox tbxDataSend;
+        private System.Windows.Forms.Button btnSendData;
+        private System.Windows.Forms.Button About;
+        private System.Windows.Forms.Button COMCheck;
+    }
+}
+
---
+++ SerialAssistant/Form1.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace SerialAssistantApplication
+{
+    public partial class Form1 : Form
+    {
+        SerialPort sp = null;//声明一个串口类
+        bool isOpen = false;//打开串口标志位
+        bool isSetProperty = false;//属性设置标志位
+        bool isHex = false;//十六进制显示标志位
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //列出可用COMPort
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+            {
+                cbbCOMPort.Items.Add(port);
+            }
+
+            //列出BaudRate
+            cbbBaudRate.Items.Add("1200");
+            cbbBaudRate.Items.Add("2400");
+            cbbBaudRate.Items.Add("4800");
+            cbbBaudRate.Items.Add("9600");
+            cbbBaudRate.Items.Add("19200");
+            cbbBaudRate.Items.Add("38400");
+            cbbBaudRate.Items.Add("43000");
+            cbbBaudRate.Items.Add("56000");
+            cbbBaudRate.Items.Add("57600");
+            cbbBaudRate.Items.Add("115200");
+            cbbBaudRate.SelectedIndex = 9;
+            //列出DateBits
+            cbbDataBits.Items.Add("8");
+            cbbDataBits.Items.Add("7");
+            cbbDataBits.Items.Add("6");
+            cbbDataBits.Items.Add("5");
+            cbbDataBits.SelectedIndex = 0;
+            //列出StopBits
+            cbbStopBits.Items.Add("0");
+            cbbStopBits.Items.Add("1");
+            cbbStopBits.Items.Add("1.5");
+            cbbStopBits.Items.Add("2");
+            cbbStopBits.SelectedIndex = 1;
+            //列出Parity
+            cbbParity.Items.Add("无");
+            cbbParity.Items.Add("奇校验");
+            cbbParity.Items.Add("偶校验");
+            cbbParity.SelectedIndex = 0;
+        }
+
+        private bool CheckPortSetting()//检查串口是否设置
+        {
+            if (cbbCOMPort.Text.Trim() == "") return false;
+            if (cbbBaudRate.Text.Trim() == "") return false;
+            if (cbbDataBits.Text.Trim() == "") return false;
+            if (cbbParity.Text.Trim() == "") return false;
+            if (cbbStopBits.Text.Trim() == "") return false;
+            return true;
+        }
+
+        private bool CheckSendData()
+        {
+            if (tbxDataSend.Text.Trim() == "") return false;
+            return true;
+        }
+
+        private void SetPortProperty()//设置串口的属性
+        {
+            sp = new SerialPort();
+            sp.PortName = cbbCOMPort.Text.Trim();//设置串口名
+            sp.BaudRate = Convert.ToInt32(cbbBaudRate.Text.Trim());//设置串口的波特率
+            float f = Convert.ToSingle(cbbStopBits.Text.Trim());//设置停止位
+            if (f == 0)
+            {
+                sp.StopBits = StopBits.None;
+            }
+            else if (f == 1.5)
+            {
+
+                sp.StopBits = StopBits.OnePointFive;
+            }
+            else if (f == 1)
+            {
+                sp.StopBits = StopBits.One;
+            }
+            else if (f == 2)
+            {
+                sp.StopBits = StopBits.Two;
+            }
+            else
+            {
+                sp.StopBits = StopBits.One;
+            }
+            sp.DataBits = Convert.ToInt16(cbbDataBits.Text.Trim());//设置数据位
+            string s = cbbParity.Text.Trim(); //设置奇偶校验位
+            if (s.CompareTo("无") == 0)
+            {
+                sp.Parity = Parity.None;
+            }
+            else if (s.CompareTo("奇校验") == 0)
+            {
+                sp.Parity = Parity.Odd;
+            }
+            else if (s.CompareTo("偶校验") == 0)
+            {
+                sp.Parity = Parity.Even;
+            }
+            else
+            {
+                sp.Parity = Parity.None;
+            }
+            sp.ReadTimeout = -1;//设置超时读取时间
+            sp.RtsEnable = true;
+            //定义 DataReceived 事件，当串口收到数据后触发事件
+            sp.DataReceived += new SerialDataReceivedEventHandler(sp_DataReceived);
+            if (rbnHEX.Checked)
+            {
+
+                isHex = true;
+            }
+            else
+            {
+                isHex = false;
+            }
+        }
+
+        private void sp_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            System.Threading.Thread.Sleep(10);//延时 100ms 等待接收完数据
+                                               //this.Invoke 就是跨线程访问 ui 的方法，也是本文的范例
+            this.Invoke((EventHandler)(delegate
+            {
+                if (isHex == false)
+                {
+                    //tbxDataRev.Text += sp.ReadLine();
+                    tbxDataRev.AppendText(sp.ReadLine());
+                }
+                else
+                {
+                    Byte[] ReceivedData = new Byte[sp.BytesToRead]; //创建接收字节数组
+                    sp.Read(ReceivedData, 0, ReceivedData.Length); //读取所接收到的数据
+                    String RecvDataText = null;
+                    for (int i = 0; i < ReceivedData.Length - 1; i++)
+                    {
+                        RecvDataText += ("0x" + ReceivedData[i].ToString("X2") + " ");
+                    }
+                    //tbxDataRev.Text += RecvDataText;
+                    tbxDataRev.AppendText(sp.ReadLine());
+                }
+                sp.DiscardInBuffer();//丢弃接收缓冲区数据
+            }));
+        }
+
+        private void About_Click(object sender, EventArgs e)
+        {
+            Form About = new AboutBox1();
+            About.ShowDialog();
+        }
+
+        private void COMCheck_Click(object sender, EventArgs e)
+        {
+            cbbCOMPort.Items.Clear();
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+            {
+                cbbCOMPort.Items.Add(port);
+            }
+        }
+
+        private void btnOpenCOM_Click(object sender, EventArgs e)
+        {
+            if (isOpen == false)
+            {
+                if (!CheckPortSetting())//检测串口设置
+                {
+                    MessageBox.Show("串口未设置！", "错误提示");
+                    return;
+                }
+
+                if (!isSetProperty)//串口未设置则设置串口
+                {
+                    SetPortProperty();
+                    isSetProperty = true;
+                }
+                try//打开串口
+                {
+                    sp.Open();
+                    isOpen = true;
+                    btnOpenCOM.Text = "关闭串口";
+                    //串口打开后则相关的串口设置按钮便不可再用
+                    cbbCOMPort.Enabled = false;
+                    cbbBaudRate.Enabled = false;
+                    cbbDataBits.Enabled = false;
+                    cbbParity.Enabled = false;
+                    cbbStopBits.Enabled = false;
+                    rbnChar.Enabled = false;
+                    rbnHEX.Enabled = false;
+                }
+                catch (Exception)
+                {
+                    //打开串口失败后，相应标志位取消
+                    isSetProperty = false;
+                    isOpen = false;
+                    MessageBox.Show("串口无效或已被占用！", "错误提示");
+                }
+            }
+            else
+            {
+                try//打开串口
+                {
+                    sp.Close();
+                    isOpen = false;
+                    isSetProperty = false;
+                    btnOpenCOM.Text = "打开串口";
+                    //关闭串口后，串口设置选项便可以继续使用
+                    cbbCOMPort.Enabled = true;
+                    cbbBaudRate.Enabled = true;
+                    cbbDataBits.Enabled = true;
+                    cbbParity.Enabled = true;
+                    cbbStopBits.Enabled = true;
+                    rbnChar.Enabled = true;
+             
+                rbnHEX.Enabled = true;
+                }
+                catch (Exception)
+                {
+                    //lblStatus.Text = "关闭串口时发生错误";
+                }
+            }
+        }
+
+       
+
+        private void btnSendData_Click(object sender, EventArgs e)
+        {
+            if (isOpen)//写串口数据
+            {
+                try
+                {
+                    sp.WriteLine(tbxDataSend.Text);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("发送数据时发生错误！", "错误提示");
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("串口未打开！", "错误提示");
+                return;
+            }
+            if (!CheckSendData())//检测要发送的数据
+            {
+                MessageBox.Show("请输入要发送的数据！", "错误提示");
+                return;
+            }
+        }
+
+        private void btnClearData_Click(object sender, EventArgs e)
+        {
+            tbxDataRev.Text = "";
+        }
+    }
+}
+
